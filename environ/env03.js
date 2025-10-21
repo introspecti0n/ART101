07c8a1badaba5abf404884c40b71fb5884d8754e
lur :p <lur.denney@gmail.com>
2025-10-20 22:08:17 -0700

    hi everyone

---
+++ environ/env03.js
@@ -0,0 +1,10 @@
+let KokoInfo = {
+    species: "clown",
+    age: 20,
+    name: "koko",
+    birthplace: "New York",
+    hairColor: "black",
+    occupation: "preformer"
+};
+
+$("#output").html(KokoInfo);
